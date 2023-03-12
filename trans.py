9ba380e140a0d987e68292628a84a483eb925646
Daniel Lopez <daniellopez@Daniels-MacBook-Air.local>
2023-03-11 21:37:04 -0600

    geojson original

--- trans.py
+++ trans.py
@@ -9,7 +9,7 @@ def desplegar_mapa():
 
     mapa = folium.Map(location=[9.748917, -83.753428], zoom_start=7.2, scrollWheelZoom = False, titles = 'Carto')
     choropleth = folium.Choropleth(
-         geo_data='map.geojson'
+         geo_data='prueba_c.geojson'
      )
     choropleth.geojson.add_to(mapa)
     st_map = st_folium(mapa, width=700, height=450)
