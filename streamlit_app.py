a7a9de9c26c97c7971926f640c0974af425739ef
gabnarmor18 <gabrielnaranjo18@gmail.com>
2023-05-03 08:48:19 -0600

    Caja de datos añadida

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,18 @@ def main():
     sexo = desarrollo.desplegar_sexo(desarrollo.incidencia)
     localizacion = desarrollo.desplegar_zona(desarrollo.incidencia)
     mapa = desarrollo.desplegar_mapa(desarrollo.incidencia, tiempo, sexo, localizacion)
-    mapa = desarrollo.desplegar_canton(desarrollo.incidencia, mapa)
+    canton = desarrollo.desplegar_canton(desarrollo.incidencia, mapa)
+
+        #Display Metrics
+    st.subheader(f'Datos de incidencia en {canton}')
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        desarrollo.desplegar_datos(desarrollo.incidencia, tiempo, sexo, canton, localizacion, '# de casos')
+    with col2:
+        desarrollo.desplegar_datos(desarrollo.incidencia, tiempo, sexo, canton, localizacion, "Porcentaje del total de casos del país")
+    with col3:
+        desarrollo.desplegar_datos(desarrollo.incidencia, tiempo, sexo, canton, localizacion, "Porcentaje de casos de cáncer en " + str(localizacion) + " en el país") 
+
 if __name__ == "__main__":
     main()
